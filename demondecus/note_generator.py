f06b48463076e06874f29a29e81ffdaa7c595cb6
James Petullo <pentester18@gmail.com>
2019-04-18 19:40:45 -0400

    Add files via upload

---
+++ demondecus/note_generator.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup as soup
+import typing, re
+
+class LineObj:
+    def __init__(self, _num:int, _octave:int, is_step:bool=False) -> None:
+        self.num, self.octave, self.is_step = _num, _octave, is_step
+    def __sub__(self, _new_note:dict) -> int:
+        if self.num >= _new_note['position']:
+            return (abs(self.num-_new_note['position'])+1)*2 - 1-int(self.is_step) +  int(_new_note['step'] == 'has_step')
+        return (abs(self.num-_new_note['position'])+1)*2 - 1-int(self.is_step) -  int(_new_note['step'] == 'has_step')
+    def __repr__(self) -> str:
+        return f'Line({self.num}, {self.octave}, step={self.is_step})'
+
+class NoteControl(type):
+    def __getitem__(cls, _val:typing.Tuple[dict, str]) -> typing.Tuple[tuple, str]:
+        if isinstance(_val, str):
+            return cls.type_converter[re.sub('^dotted_', '', _val)]
+        for a, b in cls.note_placement.items():
+            _d = b - _val
+            if _d == 1:
+                return (a, b)
+            if not _d%8:
+                return (a, LineObj(_val['position'], b.octave+(_d//8), _val['step'] == 'has_step'))
+        
+class Note(metaclass=NoteControl):
+    headers = ['step', 'octave', 'note_type']
+    note_placement = {'A':LineObj(6, 4), 'B':LineObj(6, 4, True), 'C':LineObj(5, 5), 'D':LineObj(8, 4), 'E':LineObj(8, 4, True), 'F':LineObj(7, 4), 'G':LineObj(7, 4, True)}
+    type_converter = {'eighth_note':'eighth', 'quarter_note':'quarter', 'sixteenth_note':'16th', 'half_note':'half', 'whole_note':'whole'}
+    def __init__(self, *args:typing.List[str]) -> None:
+        self.__dict__ = dict(zip(self.__class__.headers, args))
+    def __iter__(self) -> typing.Iterator:
+        yield from [getattr(self, i) for i in self.__class__.headers]
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(step={self.step}, octave={self.octave}, type={self.note_type})'
+
+class NoteGenerator:
+    @classmethod
+    def parse_file(cls, _name:str) -> typing.List[Note]:
+        return [Note(*[getattr(getattr(i, c), 'text', None) for c in ['step', 'octave', 'type']]) for i in soup(open(f'datasets/{_name}.musicxml').read(), 'html.parser').find_all('note')]
+
+if __name__ == '__main__':
+    #print([tuple(i) for i in NoteGenerator.parse_file('Chant')])
+    #print([tuple(i) for i in NoteGenerator.parse_file('MozartTrio')])
+    #print([tuple(i) for i in NoteGenerator.parse_file('DebuMandSample')])
+    print(Note[{"note":"quarter_note","line":1,"position":6,"count":4,"step":"has_step"}])
