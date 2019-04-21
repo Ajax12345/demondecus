b71007c27efe1ba559b9f43d685c9653767b5a52
James Petullo <pentester18@gmail.com>
2019-04-21 08:32:13 -0400

    Update demondecus.py

--- demondecus/demondecus.py
+++ demondecus/demondecus.py
@@ -1,10 +1,16 @@
-import flask
+import flask, note_generator, json
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
     return flask.render_template('demondecus.html')
+
+@app.route('/predict_note')
+def predict_note():
+    notes, composer = json.loads(flask.request.args.get('notes')), flask.request.args.get('composer')
+    print(notes)
+    return flask.jsonify({'result':note_generator.NoteGenerator.get_note(notes, composer)})
 
 if __name__ == '__main__':
     app.debug = True
